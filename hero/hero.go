71312f0d4121e1c3eb2cc1bf92fcf961796af04c
Anton Westman <antonwestman@gmail.com>
2018-04-09 21:21:13 +0200

    [Hero] Add jump sound

--- hero/hero.go
+++ hero/hero.go
@@ -134,3 +134,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "jump-sound"
+  type: "sound"
+  data: "sound: \"/hero/sounds/jump.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 0.2\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
